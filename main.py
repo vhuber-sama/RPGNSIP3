eda8a5eaa8d581abce00ef76ba8cd60d387211f1
vhuber-sama <117632632+vhuber-sama@users.noreply.github.com>
2024-03-15 09:52:44 +0100

    added functional ui, for it's better to have one

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import pygame as pg
 #import init
 import os
 from random import randint as rd
-import Gameobjects
 import pygame_widgets as pw
 from pygame_widgets.button import Button
+from pygame_widgets.textbox import TextBox
+from pygame_widgets.dropdown import Dropdown
+
+import Gameobjects
+import update_db
 
 pg.init()
 pg.font.init()
@@ -17,6 +21,7 @@ class Jeu:
         self.h = height
         self.halfline = height//2
         self.screen = pg.display.set_mode((self.w,self.h),vsync=1) #creates the screen object
+        
         self.clock = pg.time.Clock()
 
         if os.name == "nt":
@@ -29,13 +34,39 @@ class Jeu:
 
         self.current_location = player.zone
         self.cl_type = 'village'
-        self.chara_name = player.nom
-
-        self.texte_tuto = f"Bonjour {self.chara_name}, bienvenue au {self.cl_type} de {self.current_location}! Comme à votre habitude quand vous rentrez de mission, vous vous dirigez vers l'auberge, dans laquelle vous entrez.; Une fois accoutumé au changement de lumière et au brouhaha ambiant, vous allez au comptoir, où l'hôtesse vous salue :;'Bonsoir, {self.chara_name}. Bon retour, vous tombez à pic! Une quête vient juste d'arriver,elle sera parfaite pour vous.';'Bren, l'alchimiste qui vit à l'orée de la forêt veut aller cueillir des plantes médicinales pour ses potions, et il demande que quelqu'un l'escorte';'Il ne se passera probablement rien et au pire des cas, vous avez ce qu'il faut pour défaire les éventuelles menaces';'La récompense est de 50 pièces de bronze et une potion de soin.';change_zone;'Bonjour, vous devez être {self.chara_name}? Enchanté, je suis Bren. Merci d'avoir accepté de m'accompagner, ne perdons pas plus de temps, allons-y!';change_zone;*Alors que vous vous êtes enfoncé dans la forêt, des gobelins apparaissent*;combat;'Par Médelín, j'ai bien cru que tout cela allait mal se finir! Heureusement que vous étiez là, sinon je n'aurais pas donné cher de ma peau...';'Mais vous êtes blessé! Attendez, si je mélange ces plantes... Tenez, une potion de soin!';*Cliquez sur Inventaire, puis sur un objet utilisable pour l'utiliser*;*Une fois Bren satisfait de sa cueillette, vous le raccompagnez chez lui, puis retournez à la guilde*; Bon retour {self.chara_name}, on m a informée que vous aviez accompli votre quête avec succès!;Voici votre récompense +50 pièces de bronze, +1 potion de soin mineure"
-        self.text_to_show = self.adapte_text_v3(self.texte_tuto,65)
-        #self.ui = UI(960,960)
-
         
+        self.texte_tuto = f"Bonjour {player.nom}, bienvenue au {self.cl_type} de {self.current_location}! Comme à votre habitude quand vous rentrez de mission, vous vous dirigez vers l'auberge, dans laquelle vous entrez.; Une fois accoutumé au changement de lumière et au brouhaha ambiant, vous allez au comptoir, où l'hôtesse vous salue :;'Bonsoir, {player.nom}. Bon retour, vous tombez à pic! Une quête vient juste d'arriver,elle sera parfaite pour vous.';'Bren, l'alchimiste qui vit à l'orée de la forêt veut aller cueillir des plantes médicinales pour ses potions, et il demande que quelqu'un l'escorte';'Il ne se passera probablement rien et au pire des cas, vous avez ce qu'il faut pour défaire les éventuelles menaces';'La récompense est de 50 pièces de bronze et une potion de soin.';change_zone;'Bonjour, vous devez être {player.nom}? Enchanté, je suis Bren. Merci d'avoir accepté de m'accompagner, ne perdons pas plus de temps, allons-y!';change_zone;*Alors que vous vous êtes enfoncé dans la forêt, des gobelins apparaissent*;combat;'Par Médelín, j'ai bien cru que tout cela allait mal se finir! Heureusement que vous étiez là, sinon je n'aurais pas donné cher de ma peau...';'Mais vous êtes blessé! Attendez, si je mélange ces plantes... Tenez, une potion de soin!';*Cliquez sur Inventaire, puis sur un objet utilisable pour l'utiliser*;*Une fois Bren satisfait de sa cueillette, vous le raccompagnez chez lui, puis retournez à la guilde*; Bon retour {player.nom}, on m a informée que vous aviez accompli votre quête avec succès!;Voici votre récompense +50 pièces de bronze, +1 potion de soin mineure"
+        self.text_to_show = self.adapte_text_v3(self.texte_tuto,65)
+        
+        
+        self.new_save_btn = Button(self.screen,self.w//2-125,self.halfline + 125,256,40,text="New save",textColour=(255,255,255),inactiveColour = (120,0,0),onClick= lambda:self.launch_n_s())
+        self.load_save_btn = Button( self.screen,self.w//2-125,self.halfline + 240, 256, 40,text="Load save",textColour=(255,255,255),inactiveColour = (0,0,0))
+        self.chara_name = TextBox(self.screen,self.w//2-125,215, 500,40,font= self.font,textColour=(0,0,0),colour = (207,160,91))
+        self.espece = Dropdown(self.screen,self.w//2-125,325,500,40,name="Select species",choices=['Humain','Elfe','Nain'],colour=(207,160,91),values = ['humain','elfe','nain'])
+        self.classe = Dropdown(self.screen,self.w//2-125,525,500,40,name="Select class",choices=['Warrior','Ranger','Tank','Mage'],colour=(207,160,91),values = ['warrior','ranger','tank','mage'])        
+
+        self.spec_btn = Button(self.screen,875,325,75,40,colour = (200,20,20),text="Confirm")
+        self.spec_btn.onClick= lambda: self.confirm_choice(self.espece)
+        
+
+        self.cla_btn = Button(self.screen,875,525,75,40,colour = (200,20,20),text="Confirm")
+        self.cla_btn.onClick= lambda: self.confirm_choice(self.classe)
+        
+        self.create_btn = Button(self.screen,self.halfline-40,900,75,40,colour=(175,35,35),text="Create Character",textColour=(255,255,255),onClick= lambda: update_db.tools.create_player(1,self.args[0],self.args[1],self.args[2],10,0,1,0,0,0,0,0,0,0,0,0,False))
+
+        self.new_save_btn.hide()
+        self.load_save_btn.hide()
+        self.chara_name.hide()
+        self.espece.hide()
+        self.classe.hide()
+        self.spec_btn.hide()
+        self.cla_btn.hide()
+        self.create_btn.hide()
+        self.args = []
+        self.menu_widgets = [self.new_save_btn,self.load_save_btn]
+        self.ns_widgets = [self.chara_name,self.espece,self.classe,self.spec_btn,self.cla_btn,self.create_btn]
+
+        self.ui = self.menu_ui()
         #self.quit_btn = Button(">Quit", 10, 890, 150, 70,self.screen)
 
  
@@ -98,20 +129,57 @@ class Jeu:
         shown_ttl = self.ttl_font.render(title,1,(107,9,9))
         self.screen.blit(shown_ttl,(self.halfline-170,215))
 
-        pg.draw.rect(self.screen,(0,0,0),(2,self.halfline+2,self.w-4,self.halfline-4))
-        pg.draw.rect(self.screen,(255,255,255),(0,self.halfline,self.w,self.halfline),width=2)
+        #pg.draw.rect(self.screen,(0,0,0),(2,self.halfline+2,self.w-4,self.halfline-4))
+        #pg.draw.rect(self.screen,(255,255,255),(0,self.halfline,self.w,self.halfline),width=2)
 
-        new_save_btn = Button(self.screen,self.w//2-125,self.halfline + 125,256,40,text="New save",textColour=(255,255,255),inactiveColour = (0,0,0))
-        new_save_btn.draw()
+        for e in self.menu_widgets:
+            print("drawing ",e)
+            e.show()
+
+    def txtbox_to_str(self,textbox,var):
+        var = textbox.getText()
+        self.args.append(var) 
+
+    def confirm_choice(self,drop):
+        print(f'added{drop.getSelected()} to {self.args}')
+        self.args.append(drop.getSelected())
+    
+
+    def launch_n_s(self):
+        for e in self.menu_widgets:
+            e.hide()
+        self.ui = self.new_save_ui()
+
+    def new_save_ui(self):
         
-        
-        
-        load_save_btn = Button( self.screen,self.w//2-125,self.halfline + 240, 256, 40,text="Load save",textColour=(255,255,255),inactiveColour = (0,0,0))
-        load_save_btn.draw()
+        pg.font.init()
+
+        pg.draw.rect(self.screen,(0,0,0),(0,0,960,960))
+
+       
+        chara = ""
 
         
+        self.chara_name.onSubmit=lambda: self.txtbox_to_str(self.chara_name,chara)
+        
+        for e in self.ns_widgets:
+            e.show()
+
+        title = "Character name :"
+        shown_ttl = self.font.render(title,1,(255,255,255))
+        self.screen.blit(shown_ttl,(50,215))
+
+        spec = "Character species :"
+        shown_spec = self.font.render(spec,1,(255,255,255))
+        self.screen.blit(shown_spec,(50,325))
+
+        cla = "Character class :"
+        shown_cla = self.font.render(cla,1,(255,255,255))
+        self.screen.blit(shown_cla,(50,525))
 
 
+
+        
 #============================================================================================================================================================#
 
     def run_game(self):
@@ -132,10 +200,11 @@ class Jeu:
 
         
             self.clock.tick(60)
-            self.menu_ui()
-            
+            print(self.args)            
             quit_btn = Button(self.screen, 10, 890, 150, 70,text="Quit",textColour=(255,255,255),inactiveColour = (125,0,0),hoverColour= (255,0,0),onClick=lambda: pg.quit())
-            quit_btn.draw()
+            #quit_btn.draw()
+
+            #print(self.args)
             
             pw.update(events)
 #            test_btn = Button("Test",450,890,70,150,self.screen)
